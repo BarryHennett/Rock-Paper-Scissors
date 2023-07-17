1a2c6238f520135417da7cecd4acfad81e391bbd
BarryHennett <harrybennettnz@gmail.com>
2023-07-18 09:24:36 +1200

    Rock Paper Scissors Script

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,28 @@
+import random
+
+while True:
+    user_choice = input ("Choose between Rock, Paper, Scissors: ")
+    possible_choices = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(possible_choices)
+    print(f"n\You chose {user_choice}, Computer chose {computer_choice}.\n")
+
+    if user_choice == computer_choice:
+        print(f"Both players selected {user_choice}. It's a tie!")
+    elif user_choice == "rock":
+        if computer_choice == "scissors":
+            print("Rock smashes scissors! You Dominated!")
+        else:
+            print("Paper covers rock! You Were Defeated!?")
+    elif user_choice == "paper":
+        if computer_choice == "rock":
+            print("Paper covers rock! You Dominated!")
+        else:
+            print("Scissors cuts paper! You Were Defeated!?")
+    elif user_choice == "scissors":
+        if computer_choice == "paper":
+            print("Scissors cuts paper! You Dominated!")
+        else:
+            print("Rock smashes scissors! You Were Defeated!?")
+    play_again = input("Play Again? yes/no: ")
+    if play_again.lower() != "yes":
+        break
